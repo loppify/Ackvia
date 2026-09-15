97e1ffa9d0e19b1fc4df126a28fc15bb1adea3d2
loppify <rost.tarasov@gmail.com>
2026-09-15 11:13:23 +0200

    retry mechanism implemented. exponential backoff as delay

--- alembic/versions/85c8e434c16d_4_rewrite_states.py
+++ alembic/versions/85c8e434c16d_4_rewrite_states.py
@@ -8,8 +8,6 @@ Create Date: 2026-09-12 13:00:00.000000
 
 from typing import Sequence, Union
 
-from alembic import op
-
 # revision identifiers, used by Alembic.
 revision: str = "85c8e434c16d"
 down_revision: Union[str, Sequence[str], None] = "3d31aa2ddba6"
